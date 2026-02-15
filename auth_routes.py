a803d0e8218ffca472e30193b015e65139698b4d
Matheus Luiz Soares <matheusluiz15a@gmail.com>
2026-02-15 14:49:32 -0300

    começando os primeiros passos para criação de login exingdo o token, Part2

--- auth_routes.py
+++ auth_routes.py
@@ -4,11 +4,16 @@ from dependencies import pegar_sessao
 from fastapi import APIRouter, HTTPException, Depends
 from models import Usuario
 from main import bcrypt_context
-from schemas import UsuarioSchema
+from schemas import UsuarioSchema, loginSchema
 from sqlalchemy.orm import Session
 
 #ESSE é o meu prefixo do meu router, ou seja, todas as rotas que eu criar aqui dentro do auth_router vão começar com /auth
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
+
+def criar_token(email)
+   token = f""
+
+
 
 @auth_router.get("/")
 async def home():
@@ -37,8 +42,22 @@ async def registro(usuario_schema: UsuarioSchema, session: Session = Depends(peg
 
 
 @auth_router.post("/Login")
-async def login(usuario_schema: UsuarioSchema, session: Session = Depends(pegar_sessao));
+async def login(login_schema: loginSchema, session: Session = Depends(pegar_sessao)):
+ usuario= session.query(Usuario).filter(Usuario.email == login_schema.email).first()
+ if not usuario:
+    raise HTTPException(status_code=400, detail="Email ou senha incorretos")
+ else:
+      access_token = criar_token(usuario.id)
+      return {"access_token": access_token, "token_type": "bearer"}
+ 
 
+
+
+
+
+
+ ## acess_token é o token de acesso que será retornado para o cliente após um login bem-sucedido. Ele é criado usando a função criar_token, que gera um token único para o usuário com base em seu ID. O token_type "bearer" indica que o token é do tipo Bearer, que é um tipo comum de token de acesso usado em APIs para autenticação e autorização. O cliente pode usar esse token para acessar rotas protegidas na API, enviando-o no cabeçalho Authorization das requisições subsequentes.
+    
 
 
         
