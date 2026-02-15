b817bfd63b0d10c0057ae594e185604d5656ee42
Matheus Luiz Soares <matheusluiz15a@gmail.com>
2026-02-15 00:04:52 -0300

    ajustando os paramentros do usuario

--- auth_routes.py
+++ auth_routes.py
@@ -4,6 +4,8 @@ from dependencies import pegar_sessao
 from fastapi import APIRouter, HTTPException, Depends
 from models import Usuario
 from main import bcrypt_context
+from schemas import UsuarioSchema
+from sqlalchemy.orm import Session
 
 #ESSE é o meu prefixo do meu router, ou seja, todas as rotas que eu criar aqui dentro do auth_router vão começar com /auth
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
@@ -13,24 +15,25 @@ async def home():
     return {"message": "Voçe acessou a rota de autenticação"}
 
 @auth_router.post("/Registro")
-async def registro(email: str, senha: str, nome: str, session=Depends(pegar_sessao)):
-    usuario = session.query(Usuario).filter(Usuario.email == email).first()
+async def registro(usuario_schema: UsuarioSchema, session: Session = Depends(pegar_sessao)):
+    usuario = session.query(Usuario).filter(Usuario.email == usuario_schema.email).first()
     if usuario:
         raise HTTPException(status_code=400, detail="Email já cadastrado")
   
-    senha_criptografada = bcrypt_context.hash(senha)
+    senha_criptografada = bcrypt_context.hash(usuario_schema.senha)
 
     novo_usuario = Usuario(
-        nome=nome,
-        email=email,
+        nome=usuario_schema.nome,
+        email=usuario_schema.email,
         senha=senha_criptografada,
-        ativo=True
+        ativo=True,
+        admin=usuario_schema.admin
     )
 
     session.add(novo_usuario)
     session.commit()
 
-    return {"msg": "Usuário criado com sucesso"}
+    return {"msg": f"Usuário criado com sucesso, {usuario_schema.email}"}
 
         
 ## depends é uma função do FastAPI que permite injetar dependências em suas rotas. Nesse caso, estamos usando Depends(pegar_sessao) para injetar a sessão do banco de dados na função de registro. Isso significa que toda vez que a rota de registro for chamada, o FastAPI irá criar uma nova sessão do banco de dados e passá-la para a função de registro como um argumento.
